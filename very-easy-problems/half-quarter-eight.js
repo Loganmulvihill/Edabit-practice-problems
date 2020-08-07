72a94e5b28e7790cfef89d0ff160660a84c8b3e3
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-06 20:31:36 -0500

    completed half, quarter, and eigth function

---
+++ very-easy-problems/half-quarter-eight.js
@@ -0,0 +1,10 @@
+function halfQuarterEighth(n) {
+  let array = [];
+  const half = n / 2;
+  const quarter = n / 4;
+  const eigth = n / 8;
+  array.push(half);
+  array.push(quarter);
+  array.push(eigth);
+  return array;
+}
