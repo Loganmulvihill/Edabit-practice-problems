0bd1c8409f7a6db1c2855881213b7f300c774269
loganmulvihill <mulvihill.logan@gmail.com>
2020-08-22 21:02:04 -0500

    completed Luke I am your father function

---
+++ luke-i-am.js
@@ -0,0 +1,14 @@
+function relationToLuke(name) {
+  if (name === "Darth Vader") {
+    return `Luke, I am your father.`
+  }
+  if (name === "Leia") {
+    return `Luke, I am your sister.`
+  }
+  if (name === "Han") {
+    return `Luke, I am your brother in law.`
+  }
+  if (name === "R2D2") {
+    return `Luke, I am your droid.`
+  }
+}
